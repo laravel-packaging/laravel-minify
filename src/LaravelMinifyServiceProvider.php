d627dbbc34c0c0b691710efdf8e1a56bcadca804
Mehrdad Hajiyani <mehrdad.hajiyani@gmail.com>
2025-06-09 00:07:34 +0330

    Initialize

---
+++ src/LaravelMinifyServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LaravelPackaging\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMinifyServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+    }
+}
